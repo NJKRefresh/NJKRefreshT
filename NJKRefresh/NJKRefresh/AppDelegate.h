8add3f9e29234149d723fa43b627a0384cbf794f
yanghengwei <514068922@qq.com>
2018-03-08 16:35:01 +0800

    1

---
+++ NJKRefresh/NJKRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NJKRefresh
+//
+//  Created by iOS on 2018/3/8.
+//  Copyright © 2018年 NJK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NJKRefresh/NJKRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NJKRefresh
+//
+//  Created by iOS on 2018/3/8.
+//  Copyright © 2018年 NJK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
